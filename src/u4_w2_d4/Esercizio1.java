adb9d71640e0e0fb08ef8f1a86985f6b40dd01e0
francescocaccia <123968277+francescocaccia@users.noreply.github.com>
2023-05-11 21:10:10 +0200

    esercizio

--- src/u4_w2_d4/Esercizio1.java
+++ src/u4_w2_d4/Esercizio1.java
@@ -9,14 +9,19 @@ public class Esercizio1 {
 
 		Product newProduct = new Product(123L, "libro", "book", 230.0);
 		Product newProduct1 = new Product(124L, "telefono", "ultimaGenerazione", 220.0);
-		Product newProduct2 = new Product(125L, "PC", "Mac", 12.0);
-		Product newProduct3 = new Product(126L, "bottiglia", "acqua", 22.0);
+		Product newProduct2 = new Product(125L, "PC", "baby", 12.0);
+		Product newProduct3 = new Product(126L, "bottiglia", "baby", 22.0);
 
 		List<Product> myList = List.of(newProduct, newProduct1, newProduct2, newProduct3);
 		List<Product> listaDue = myList.stream()
 				.filter(prodotto -> prodotto.getCategory().equals("book") && prodotto.getPrice() > 100)
 				.collect(Collectors.toList());
 		System.out.println(listaDue.size());
+
+		List<Product> secondList = List.of(newProduct, newProduct1, newProduct2, newProduct3);
+		List<Product> mySecondList = secondList.stream().filter(prodotto -> prodotto.getCategory().equals("baby"))
+				.collect(Collectors.toList());
+		System.out.println(mySecondList.size());
 	}
 
 }
